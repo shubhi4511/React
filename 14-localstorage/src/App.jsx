992da067c840ae2ad42311deb2b9c6701dccc6f7
Shubhi Singh <sshubhi197@gmail.com>
2026-05-05 18:45:41 +0530

    second  commit

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
 
 const App = () => {
+
+//setItem
+  localStorage.setItem('user','shubhi')
+
+//getItem
+  const age = localStorage.getItem('age')
+
+//removeItem
+  localStorage.removeItem('user')
+
+  console.log(age);
+
+LocalStorage.clear('user')
+
+  const user = {
+    name:'Shubhi',
+    age:22,
+    city:'Noida'
+  }
+
+  localStorage.setItem('user', JSON.stringify('user'))
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  console.log(typeof(user));
+
+  // const user = JSON.parse(localStorage.getItem('user'))
+
+  // console.log(user);
+
+
   return (
     <div>
       App
