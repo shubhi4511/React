bb9aee11ce382d4d3c9497a60c985eafbb4ada59
Shubhi Singh <sshubhi197@gmail.com>
2026-06-03 12:09:27 +0530

    second  commit

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,61 +1,66 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Card from './components/Card';
 
 const App = () => {
 
-  const [userData, setUserData] = useState([])
+  const [userData, setUserData] = useState([]);
+
+  const [index, setIndex] = useState(1)
 
   const getData = async () => {
-    const response = await axios.get(
-      'https://picsum.photos/v2/list?page=2&limit=30'
-    )
-
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
     setUserData(response.data)
   }
-  let printUserData = 'No User Available';
 
-  if(userData.length>0) {
-    return <div>
-      <img src={Element.download_url} alt=""/>
-    </div>
+  useEffect(function () {
+    getData()
+  }, [index])
+
+
+  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+
+      return <div key={idx}>
+        <Card elem={elem} />
+      </div>
+    })
   }
 
   return (
-    <div className='bg-black overflow-auto min-h-screen text-white p-5'>
-
-      <button
-        onClick={getData}
-        className='bg-green-600 px-5 py-2 rounded mb-5 active:scale-95'
-      >
-        Get Data
-      </button>
-
-      <div className='grid grid-cols-3 gap-5'>
-
-        {userData.length > 0
-          ? userData.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className='bg-zinc-900 p-3 rounded'
-                >
-                  <img
-                    src={item.download_url}
-                    alt=""
-                    className='w-full h-52 object-cover rounded'
-                  />
-
-                  <h1 className='text-xl mt-3 font-bold'>
-                    {item.author}
-                  </h1>
-                </div>
-              )
-            })
-          : <h1>No Data Found</h1>
-        }
-
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      <div className='flex h-[82%] flex-wrap gap-4 p-2'>
+        {printUserData}
       </div>
 
+      <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+          style={{ opacity: index == 1 ? 0.6 : 1 }}
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+          onClick={() => {
+            if (index > 1) {
+              setIndex(index - 1)
+              setUserData([])
+            }
+          }}
+        >
+          Prev
+        </button>
+        <h4>Page {index}</h4>
+        <button
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+          onClick={() => {
+            setUserData([])
+
+            setIndex(index + 1)
+
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
